ed904580f0e8b8798c56a1ec72c88fcc866b856d
Emmanuel <emmanuelcu@gmail.com>
2015-08-20 00:06:31 -0500

    third commit

---
+++ UITableViewController Challenge Solution/OutSpaceImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  OutSpaceImageViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Emmanuel Cuevas on 8/16/15.
+//  Copyright (c) 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OutSpaceImageViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/OutSpaceImageViewController.m
@@ -0,0 +1,13 @@
+//
+//  OutSpaceImageViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Emmanuel Cuevas on 8/16/15.
+//  Copyright (c) 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import "OutSpaceImageViewController.h"
+
+@implementation OutSpaceImageViewController
+
+@end
